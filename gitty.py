e509dd1288a953b65df64ec7bed6fe82b5aa48be
Tom PoLáKoSz <polakosz@freemail.hu>
2019-09-12 09:42:36 +0200

    Implement joke function

--- gitty.py
+++ gitty.py
@@ -1,2 +1,16 @@
 def introduce():
     print('Hello, I\'m Gitty!')
+
+def joke():
+    print('\nA husband and wife were dining at a 5-star restaurant. ' +
+        'When their food arrived, the husband said:')
+    print('- “Our food has arrived! Let’s eat!”')
+    print('His wife reminded him:')
+    print('- “Honey, you always say your prayers at home before your dinner!”')
+    print('Her husband replied:')
+
+    input('\nPress any key to continue\n')
+
+    print('“That’s at home, my dear. Here the chef knows how to cook…”')
+
+joke()
